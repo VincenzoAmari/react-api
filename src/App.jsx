9357fdb6773079145a0c48bd1e7047230de21534
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-26 17:37:28 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
-import "./index.css";
-
-
+import PostList from "./components/PostList";
 
 function App() {
-  
- 
+  return (
+    <div>
+      <h1>Blog</h1>
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -14,7 +14,27 @@ const PostList = () => {
         .catch((error) => console.error("Errore nel recupero dei post:", error));
     }, []);
 
-    return (  );
+    return (<div>
+        <h2>Lista Post</h2>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Titolo</th>
+              <th>Contenuto</th>
+              <th>Tags</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => (
+              <tr key={post.id}>
+                <td>{post.titolo}</td>
+                <td>{post.contenuto}</td>
+                <td>{post.tags.join(", ")}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>  );
 };
 
 export default PostList;
